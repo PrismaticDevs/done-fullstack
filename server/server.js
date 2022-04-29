037b72431b33490ec11bb0be4372d27d77854d57
Matthew Brignola <matthewbrignola@ymail.com>
2022-04-29 14:08:34 -0600

    built login form using vanilla js. login function not added yet

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 
 const routes = require("./routes");
 const cors = require("cors");
 const { db } = require("./connection");
+const { log } = require("console");
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,5 +15,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+});
 
 app.listen(PORT, () => console.log(`App listening on ${PORT}`));
