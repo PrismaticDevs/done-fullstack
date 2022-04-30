5936919710d6161dcbd23f72afa52b9866cdf34d
Matthew Brignola <matthewbrignola@ymail.com>
2022-04-29 20:25:58 -0600

    Revert "built login form using vanilla js. login function not added yet"
    This reverts commit 037b72431b33490ec11bb0be4372d27d77854d57.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-const path = require("path");
 
 const routes = require("./routes");
 const cors = require("cors");
 const { db } = require("./connection");
-const { log } = require("console");
 
 const PORT = process.env.PORT || 3001;
 
@@ -15,10 +13,5 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
-
-app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
-});
 
 app.listen(PORT, () => console.log(`App listening on ${PORT}`));
