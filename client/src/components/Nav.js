d8004bb830f6bfe987c4dba60d0ef4296eb6eb9c
Matthew Brignola <matthewbrignola@ymail.com>
2022-05-03 15:44:45 -0600

    done logo removed

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -12,7 +12,7 @@ import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import { Link } from "@mui/material";
 
-import png from "../img/done.png";
+import png from "../img/laptop.png";
 
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
